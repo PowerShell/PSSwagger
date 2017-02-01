b672176954a600c0d297594bb9291608332366a9
Bryan Wang (UPS) <brywang@microsoft.com>
2017-02-01 14:59:56 -0800

    Fix for scenario test from latest merge

--- Tests/PSSwaggerScenario.Tests.ps1
+++ Tests/PSSwaggerScenario.Tests.ps1
@@ -4,15 +4,22 @@ Describe "Basic API" -Tag ScenarioTest {
         # Ensure PSSwagger isn't loaded (including the one installed on the machine, if any)
         Get-Module PSSwagger | Remove-Module
 
+        $testSpec = "PsSwaggerTestBasicSpec.json"
+        $testData = "PsSwaggerTestBasicData.json"
+        $testRoutes = "PsSwaggerTestBasicRoutes.json"
+
         # Import PSSwagger
         Import-Module "$PSScriptRoot\..\PSSwagger\PSSwagger.psd1" -Force
 
         $generatedModulesPath = Join-Path -Path "$PSScriptRoot" -ChildPath "Generated"
         $testCaseDataLocation = "$PSScriptRoot\Data\PsSwaggerTestBasic"
 
+        # Note: This only works if these tests are never run in parallel, but our current usage of json-server is the same way, so...
+        $global:testDataSpec = ConvertFrom-Json ((Get-Content (Join-Path $testCaseDataLocation $testSpec)) -join [Environment]::NewLine) -ErrorAction Stop
+        
         # Generate module
         Write-Host "Generating module"
-        Export-CommandFromSwagger -SwaggerSpecPath "$testCaseDataLocation\PsSwaggerTestBasicSpec.json" -Path "$generatedModulesPath" -ModuleName "Generated.Basic.Module"
+        Export-CommandFromSwagger -SwaggerSpecPath "$testCaseDataLocation\$testSpec" -Path "$generatedModulesPath" -ModuleName "Generated.Basic.Module"
 
         # Import generated module
         Write-Host "Importing module"
@@ -20,7 +27,7 @@ Describe "Basic API" -Tag ScenarioTest {
         Import-Module "$PSScriptRoot\Generated\Generated.Basic.Module\2017.1.1\Generated.Basic.Module.psd1"
         
         # Copy json-server data since it's updated live
-        Copy-Item "$testCaseDataLocation\PsSwaggerTestBasicData.json" "$PSScriptRoot\NodeModules\db.json" -Force
+        Copy-Item "$testCaseDataLocation\$testData" "$PSScriptRoot\NodeModules\db.json" -Force
 
         # Start json-server
         # TODO: Pick a port in a certain range that's open instead of hardcoding to 3000, replace the URI in Swagger spec  - See issues/16
@@ -31,7 +38,7 @@ Describe "Basic API" -Tag ScenarioTest {
             $nodeProcesses = @()
         }
 
-        $jsonServerProcess = Start-Process -FilePath "$PSScriptRoot\NodeModules\json-server.cmd" -ArgumentList "--watch `"$PSScriptRoot\NodeModules\db.json`" --routes `"$testCaseDataLocation\PsSwaggerTestBasicRoutes.json`"" -PassThru -WindowStyle Hidden
+        $jsonServerProcess = Start-Process -FilePath "$PSScriptRoot\NodeModules\json-server.cmd" -ArgumentList "--watch `"$PSScriptRoot\NodeModules\db.json`" --routes `"$testCaseDataLocation\$testRoutes`"" -PassThru -WindowStyle Hidden
         $nodeProcessToStop = Get-Process -Name "node" -ErrorAction SilentlyContinue | Where-Object {-not $nodeProcesses.Contains($_)}
         while ($nodeProcessToStop -eq $null) {
             $nodeProcessToStop = Get-Process -Name "node" -ErrorAction SilentlyContinue | Where-Object {-not $nodeProcesses.Contains($_)}
@@ -48,6 +55,10 @@ Describe "Basic API" -Tag ScenarioTest {
 
         Mock Get-AzSubscriptionId -ModuleName Generated.Basic.Module {
             return "Test"
+        }
+
+        Mock Get-AzResourceManagerUrl -ModuleName Generated.Basic.Module {
+            return "$($global:testDataSpec.schemes[0])://$($global:testDataSpec.host)"
         }
 
         It "Basic test" {
