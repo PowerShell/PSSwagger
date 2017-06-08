4b0e31a7aafd4ee4d73bb15ef665843bc4f158f1
Bryan Wang (PSHCT) <brywang@microsoft.com>
2017-06-08 13:06:34 -0700

    Move test result uploading to script #1

---
+++ Tests/Send-AppVeyorResults.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $appVeyorUrl,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $testResultRootDir,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $testResultFilePattern = "*TestResults.xml"
+)
+
+Write-Host "Uploading test results to AppVeyor: $appVeyorUrl"
+Get-ChildItem -Path $testResultRootDir -Filter $testResultFilePattern -File -Recurse | ForEach-Object {
+    Write-Host "Uploading file: $($_.FullName)"
+    (New-Object 'System.Net.WebClient').UploadFile($appVeyorUrl, "$($_.FullName)")
+}
