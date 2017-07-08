a6919bd4bfe625ad8f52d48ce844cd6f0fb11c8b
Bryan Wang (PSHCT) <brywang@microsoft.com>
2017-07-07 23:30:09 -0700

    Logging

--- Tests/PSSwaggerScenario.Tests.ps1
+++ Tests/PSSwaggerScenario.Tests.ps1
@@ -27,9 +27,13 @@ Describe "Basic API" -Tag ScenarioTest {
         $processes = Start-JsonServer -TestRootPath $PSScriptRoot -TestApiName "PsSwaggerTestBasic" -TestRoutesFileName "PsSwaggerTestBasicRoutes.json" -Verbose
         Write-Verbose -Message "Tracing: $($global:PSSwaggerTest_EnableTracing)"
         if ($global:PSSwaggerTest_EnableTracing) {
+            Write-Verbose -Message 'Initialize PSSwaggerDependencies'
             Initialize-PSSwaggerDependencies
+            Write-Verbose -Message 'Import PSSwaggerTestTracing'
             Import-Module "$PSScriptRoot\PSSwaggerTestTracing.psm1"
+            Write-Verbose -Message 'AddTracingInterceptor'
             [Microsoft.Rest.ServiceClientTracing]::AddTracingInterceptor((New-PSSwaggerTestClientTracing))
+            Write-Verbose -Message 'IsEnabled'
             [Microsoft.Rest.ServiceClientTracing]::IsEnabled = $true
         }
     }
