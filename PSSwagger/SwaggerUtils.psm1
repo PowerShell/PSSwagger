8840f71bd58f5a6f7364c8316c9082af7d615cdd
Manikyam Bavandla <manikb@microsoft.com>
2017-09-21 13:03:14 -0700

    Added support for $ParameterJsonObject.AdditionalProperties.Items.'$ref'

--- PSSwagger/SwaggerUtils.psm1
+++ PSSwagger/SwaggerUtils.psm1
@@ -991,12 +991,27 @@ function Get-ParamType
                 if((Get-Member -InputObject $ParameterJsonObject.AdditionalProperties -Name 'Type') -and
                    ($ParameterJsonObject.AdditionalProperties.Type -eq 'array'))
                 {
-                    if((Get-Member -InputObject $ParameterJsonObject.AdditionalProperties -Name 'Items') -and
-                       (Get-Member -InputObject $ParameterJsonObject.AdditionalProperties.Items -Name 'Type') -and
-                       $ParameterJsonObject.AdditionalProperties.Items.Type)
-                    { 
-                        $ItemsType = Get-PSTypeFromSwaggerObject -JsonObject $ParameterJsonObject.AdditionalProperties.Items
-                        $paramType = "System.Collections.Generic.Dictionary[[string],[System.Collections.Generic.List[$ItemsType]]]"
+                    if(Get-Member -InputObject $ParameterJsonObject.AdditionalProperties -Name 'Items')
+                    {
+                        if((Get-Member -InputObject $ParameterJsonObject.AdditionalProperties.Items -Name 'Type') -and
+                           $ParameterJsonObject.AdditionalProperties.Items.Type)
+                        { 
+                            $ItemsType = Get-PSTypeFromSwaggerObject -JsonObject $ParameterJsonObject.AdditionalProperties.Items
+                            $paramType = "System.Collections.Generic.Dictionary[[string],[System.Collections.Generic.List[$ItemsType]]]"
+                        }
+                        elseif((Get-Member -InputObject $ParameterJsonObject.AdditionalProperties.Items -Name '$ref') -and
+                               $ParameterJsonObject.AdditionalProperties.Items.'$ref')
+                        {
+                            $ReferenceTypeValue = $ParameterJsonObject.AdditionalProperties.Items.'$ref'
+                            $ReferenceTypeName = Get-CSharpModelName -Name $ReferenceTypeValue.Substring( $( $ReferenceTypeValue.LastIndexOf('/') ) + 1 )
+                            $ItemsType = $DefinitionTypeNamePrefix + "$ReferenceTypeName"
+                            $paramType = "System.Collections.Generic.Dictionary[[string],[System.Collections.Generic.List[$ItemsType]]]"
+                        }
+                        else
+                        {
+                            $Message = $LocalizedData.UnsupportedSwaggerProperties -f ('ParameterJsonObject', $($ParameterJsonObject | Out-String))
+                            Write-Warning -Message $Message
+                        }
                     }
                     else
                     {
