fc4e7af62ed2845beceeff1ca974a09fef90b4d5
Bryan Wang (PSHCT) <brywang@microsoft.com>
2018-01-16 13:30:17 -0800

    Actually, separate -TestSuite and -Tag

--- Tests/run-tests.ps1
+++ Tests/run-tests.ps1
@@ -9,7 +9,9 @@
 #########################################################################################
 [CmdletBinding()]
 param(
+    [ValidateSet("All", "UnitTest", "ScenarioTest")]
     [string[]]$TestSuite = "All",
+    [string]$Tag,
     [string[]]$TestName,
     [ValidateSet("net452", "netstandard1.7")]
     [string]$TestFramework = "net452",
@@ -155,7 +157,7 @@ $srcPath += [System.IO.Path]::DirectorySeparatorChar
 $executeTestsCommand += @"
     ;`$verbosepreference=`'continue`';
     `$env:PSModulePath=`"$srcPath;$modulePath;`$env:PSModulePath`";
-    Invoke-Pester -Script `'$PSScriptRoot`' -ExcludeTag KnownIssue -OutputFormat NUnitXml -OutputFile ScenarioTestResults.xml -Verbose;
+    Invoke-Pester -Script `'$PSScriptRoot`' -ExcludeTag KnownIssue -OutputFormat NUnitXml -OutputFile ScenarioTestResults.xml -Verbose
 "@
 # Set up Pester params
 $pesterParams = @{'ExcludeTag' = 'KnownIssue'; 'OutputFormat' = 'NUnitXml'; 'OutputFile' = 'TestResults.xml'}
@@ -163,14 +165,15 @@ if ($PSBoundParameters.ContainsKey('TestName')) {
     $executeTestsCommand += " -TestName `"$TestName`""
 }
 
-if ($TestSuite.Contains("All")) {
+if (-not $Tag) {
     Write-Verbose "Invoking all tests."
 }
 else {
-    Write-Verbose "Running only tests with tag: $TestSuite"
-    $executeTestsCommand += " -Tag $TestSuite"
+    Write-Verbose "Running only tests with tag: $Tag"
+    $executeTestsCommand += " -Tag $Tag"
 }
 
+$executeTestsCommand += ";"
 Write-Verbose "Dependency versions:"
 Write-Verbose " -- AzureRM.Profile: $($azureRmProfile.Version)"
 Write-Verbose " -- Pester: $((get-command invoke-pester).Version)"
